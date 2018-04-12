8b112e24759d9b1593007f6e336448f13e51b1f0
Ernest Okot <okot08@gmail.com>
2018-04-13 02:07:50 +0300

    fixed lint issue

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.start = () => {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
+boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
