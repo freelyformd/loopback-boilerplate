db4724340452effb9dee7ad55d9812e23287ccec
Ernest Okot <okot08@gmail.com>
2018-04-22 18:57:58 +0300

    conditionally adding passport models

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,29 +1,28 @@
 'use strict';
 
-
 module.exports = function enableAuthentication(app) {
   // enable authentication
   app.enableAuth();
 
-  if (!process.env.ENABLE_PASSPORT_AUTH) return;
+  if (process.env.ENABLE_PASSPORT_AUTH === 'true') {
+    const {PassportConfigurator} = require('loopback-component-passport');
 
-  const {PassportConfigurator} = require('loopback-component-passport');
+    const passportConfigurator = new PassportConfigurator(app);
 
-  const passportConfigurator = new PassportConfigurator(app);
+    passportConfigurator.init();
+    passportConfigurator.setupModels({
+      userModel: app.models.Person,
+      userIdentityModel: app.models.UserIdentity,
+      userCredentialModel: app.models.UserCredential,
+    });
 
-  passportConfigurator.init();
-  passportConfigurator.setupModels({
-    userModel: app.models.Person,
-    userIdentityModel: app.models.UserIdentity,
-    userCredentialModel: app.models.UserCredential,
-  });
+    const config = require('../providers.local.js');
 
-  const config = require('../providers.local.js');
-
-  Object.keys(config).forEach(strategyName => {
-    const strategy = config[strategyName];
-    strategy.session = strategy.session !== false;
-    passportConfigurator.configureProvider(strategyName, strategy);
-    console.log('Configured %s passport strategy', strategyName);
-  });
+    Object.keys(config).forEach(strategyName => {
+      const strategy = config[strategyName];
+      strategy.session = strategy.session !== false;
+      passportConfigurator.configureProvider(strategyName, strategy);
+      console.log('Configured %s passport strategy', strategyName);
+    });
+  }
 };
