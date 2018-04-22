95d7545d8fb0e2c785cf5b5c69053ee71c534180
Ernest Okot <okot08@gmail.com>
2018-04-22 17:16:42 +0300

    using callback url from environment variables

--- server/providers.local.js
+++ server/providers.local.js
@@ -6,13 +6,19 @@ module.exports = {
     'module': 'passport-facebook',
     'clientID': process.env.FACEBOOK_CLIENT_ID,
     'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback',
+    'callbackURL': process.env.FACEBOOK_CALLBACK_URL,
     'authPath': '/auth/facebook',
     'callbackPath': '/auth/facebook/callback',
     'successRedirect': '/auth/account',
     'scope': [
       'email',
     ],
+    profileToUser: (_, profile) => {
+      const names = profile.displayName.match(/(\w+)\s+(.+)/);
+      const [__, firstname, lastname] = names;
+      // TODO: Add es6 support to eslint
+      return {firstname, lastname, ...profile};
+    },
   },
   'google-link': {
     'provider': 'google',
@@ -20,7 +26,7 @@ module.exports = {
     'strategy': 'OAuth2Strategy',
     'clientID': process.env.GOOGLE_CLIENT_ID,
     'clientSecret': process.env.GOOGLE_CLIENT_SECRET,
-    'callbackURL': 'http://localhost:3000/link/google/callback',
+    'callbackURL': process.env.GOOGLE_CALLBACK_URL,
     'authPath': '/link/google',
     'callbackPath': '/link/google/callback',
     'successRedirect': '/link/account',
