7dd0e8e2b61708ccf101a6fd7be78d3fd5174f6b
Ernest Okot <okot08@gmail.com>
2018-04-25 12:42:46 +0300

    fix(passport): removed passport integration

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -3,27 +3,4 @@
 module.exports = app => {
   // enable authentication
   app.enableAuth();
-
-  if (process.env.ENABLE_PASSPORT_AUTH === 'true') {
-    const {PassportConfigurator} = require('loopback-component-passport');
-
-    const passportConfigurator = new PassportConfigurator(app);
-
-    passportConfigurator.init();
-    passportConfigurator.setupModels({
-      userModel: app.models.Person,
-      userIdentityModel: app.models.UserIdentity,
-      userCredentialModel: app.models.UserCredential,
-    });
-
-    const config = require('../providers.local.js');
-
-    Object.keys(config).forEach(strategyName => {
-      const strategy = config[strategyName];
-      strategy.session = strategy.session !== false;
-      passportConfigurator.configureProvider(strategyName, strategy);
-      // eslint-disable-next-line no-console
-      console.log('Configured %s passport strategy', strategyName);
-    });
-  }
 };
